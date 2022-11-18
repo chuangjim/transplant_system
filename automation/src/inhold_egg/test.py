a879e0281cfc924e068830cd3157ec996e0c5a13
chuangjim <a50202jim@gmail.com>
2022-11-18 13:18:05 +0800

    test camera connection

--- automation/src/inhold_egg/test.py
+++ automation/src/inhold_egg/test.py
@@ -2,9 +2,12 @@ import cv2
 import time 
 # 選擇第二隻攝影機
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+i=0
 print(cap)
 while(True):
     # 從攝影機擷取一張影像
+    # cap.set(3,1920)
+    # cap.set(4, 1080)
     ret, frame = cap.read()
     if ret:
         # 顯示圖片
@@ -22,8 +25,22 @@ while(True):
         except:
             pass
     # 若按下 q 鍵則離開迴圈
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    k = cv2.waitKey(1)
+    if k==27:
+        print('exit')
         break
+    elif k==ord('s'):
+        cv2.imwrite('./test/'+str(i)+'.jpg', frame) 
+        print(f'save image {i}.jpg')
+        i+=1
+
+    elif k==ord('r'):
+                    # 釋放攝影機
+        cap.release()
+        # 關閉所有 OpenCV 視窗
+        cv2.destroyAllWindows()
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        print('try to reconnect')
 
 # 釋放攝影機
 cap.release()
